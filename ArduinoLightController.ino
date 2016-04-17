14b50f32129bb277fc1438baeea027746ce3fc66
Gerathain <mark@the-ellwoods.org>
2016-04-17 02:20:19 +0100

    Started adding ability to control remotely
    The arduino now reads the serial data in and uses it to change the mode
    the lights are in. Currently supports off, random variation and colour
    cycle.
    Also removed unused code from the RGBConverter as I am not using HSL at
    all.

--- ArduinoLightController.ino
+++ ArduinoLightController.ino
@@ -9,21 +9,33 @@
 double hsv[3] = {0.5, 1, 1};
 byte rgb[3];
 double vector = 0;
+int mode = 1;
 
 void setup() {
   // put your setup code here, to run once:
 
   randomSeed(analogRead(0));
 
+  Serial.begin(9600);
+  
   pinMode(RED, OUTPUT);
   pinMode(GREEN, OUTPUT);
   pinMode(BLUE, OUTPUT);
 }
 
-void loop() {
-  RGBConverter::hsvToRgb(hsv[0], hsv[1], hsv[2], rgb);
+void off()
+{
+  hsv[0] = 0;
+  hsv[1] = 0;
+  hsv[2] = 0;
+}
 
-  float r = FLOAT_RAND(0.002);
+void signalColour()
+{
+  hsv[1] = 1;
+  hsv[2] = 1;
+  
+    float r = FLOAT_RAND(0.002);
   if (FLOAT_RAND(1) >= 0.5)
   {
     r *= -1;
@@ -33,12 +45,40 @@ void loop() {
   hsv[0] += vector;
   if (hsv[0] > 1) { hsv[0] -= 1; }
   if (hsv[0] < 0) { hsv[0] += 1; }
-  
-  if(hsv[0] > 1)
+
+  return;
+}
+
+void loopColour()
+{
+  hsv[0] += 0.001;
+  hsv[1] = 1;
+  hsv[2] = 1;
+  return;
+}
+
+void loop() {
+  if(Serial.available() > 0)
   {
-    hsv[0] -= 1;
+    mode = Serial.read();
   }
-  
+
+  switch(mode)
+  {
+    case 0:
+      off();
+      break;
+    case 1:
+      signalColour();
+      break;
+    case 2:
+      loopColour();
+      break;
+    default:
+      mode = 0;
+  }  
+
+  RGBConverter::hsvToRgb(hsv[0], hsv[1], hsv[2], rgb);
   analogWrite(RED, rgb[0]);
   analogWrite(GREEN, rgb[1]);
   analogWrite(BLUE, rgb[2]);
